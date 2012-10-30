d736f5f44d421dd05082c911ec1eac81f04b44e6
alonso523 <alonso523@outlook.com>
2012-10-29 19:39:37 -0600

    Vistas y templates, parte del tutorial 3, Django

--- encuestas/settings.py
+++ encuestas/settings.py
@@ -107,7 +107,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/alonso/Escritorio/encuestas/mytemplates/admin"
+    #"/home/alonso/Escritorio/encuestas/mytemplates/admin"
+    "/home/alonso/Escritorio/encuestas/mytemplates"
 )
 
 INSTALLED_APPS = (
